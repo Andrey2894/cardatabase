432c4108fa146367d7a63b37e37ff0f6b0bc2121
Andrey2894 <Lean09@yandex.ru>
2024-11-02 20:31:20 +0300

    1

--- src/main/java/com/example/cardatabase/SecurityConfig.java
+++ src/main/java/com/example/cardatabase/SecurityConfig.java
@@ -5,11 +5,13 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -56,9 +58,5 @@ public class SecurityConfig{
         return new InMemoryUserDetailsManager(userDetails);
     }
 
-//    @Bean
-//    public AuthenticationManager
-//    getAuthenticationManager() throws Exception {
-//        return authenticationManager();
-//    }
+
 }
